e683661550b1d93de4ef5689f3202a8bc162a5db
amata7 <adrianmata@live.com>
2021-05-22 23:12:37 -0700

    adding hero component

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -1,5 +1,12 @@
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">React Google Book Search</h1>
+        <p class="lead">Search for and Save your favorite books!</p>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
--- client/src/components/SearchPage/index.js
+++ client/src/components/SearchPage/index.js
@@ -1,14 +1,14 @@
 import Hero from "../Hero";
 import SearchForm from "../SearchForm";
 import BookList from "../BookList";
-
+import Wrapper from "../Wrapper.js";
 function SearchPage() {
   return (
-    <>
+    <Wrapper>
       <Hero />
       <SearchForm />
       <BookList />
-    </>
+    </Wrapper>
   );
 }
 
