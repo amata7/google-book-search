802503f0e6b8f508dbad7193ca076b388ad55f77
amata7 <adrianmata@live.com>
2021-05-23 03:05:48 -0700

    created SearchForm component

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -1,5 +1,18 @@
 function SearchForm() {
-  return <div>SearchForm</div>;
+  return (
+    <div classname="card">
+      <div classname="card-body">
+        <h5 classname="card-title">Book Search</h5>
+        <form className="d-flex flex-column">
+          <div className="form-group">
+            <label htmlFor="search">Book</label>
+            <input type="text" className="form-control" id="search" />
+          </div>
+          <button className="btn btn-primary ml-auto">Search</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default SearchForm;
